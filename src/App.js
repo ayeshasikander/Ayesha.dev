64617b62610db21a01ca1bb011ecebe9410360fb
Ayesha_Frontend_Web <160005701+Ayesha-Sikander-724@users.noreply.github.com>
2024-08-20 18:31:34 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Main } from './pages/main/Main';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <ChakraProvider>
+      <Main />
+      </ChakraProvider>
   );
 }
 
---
+++ src/pages/main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../../layout/header/Header'
+
+export const Main = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
