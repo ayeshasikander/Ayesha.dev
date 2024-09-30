22b474b76ebafb6a1d8ef3b283b7067b99d5eef2
Ayesha_Frontend_Web <160005701+Ayesha-Sikander-724@users.noreply.github.com>
2024-09-30 15:48:44 +0500

    add functionality to snd message using contact box

--- src/pages/contact/ContactBox.jsx
+++ src/pages/contact/ContactBox.jsx
@@ -1,27 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../style/contactLayout/contact.scss";
+import emailjs from "emailjs-com";
 
 const ContactBox = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+      email: formData.email,
+      message: formData.message,
+    };
+
+    emailjs
+      .send(
+        "service_tk4sflr",
+        "template_071c7q5",
+        templateParams,
+        "XlUwAte7CY-nvQw-m"
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          alert(
+            "Thank you for your message! I will do my best to reply to you within 12 hours. Looking forward to it! :)"
+          );
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          alert("Failed to send message.");
+        }
+      );
+
+    setFormData({
+      email: "",
+      message: "",
+    });
+  };
+
   return (
     <div>
       <div className="form-container">
-        <form className="form">
+        <form className="form" onSubmit={sendEmail}>
           <div className="form-group">
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email" required="" placeholder="your mail"/>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              placeholder="Your email"
+            />
           </div>
+
           <div className="form-group">
-            <label for="textarea">How Can We Help You?</label>
+            <label htmlFor="message">How Can We Help You?</label>
             <textarea
-              name="textarea"
-              id="textarea"
+              name="message"
+              id="message"
+              value={formData.message}
+              onChange={handleChange}
               rows="10"
               cols="50"
-              required=""
-            >
-              {" "}
-            </textarea>
+              required
+              placeholder="Your message"
+            />
           </div>
+
           <button className="form-submit-btn" type="submit">
             Submit
           </button>
